64a6ee3d343e4580c8a69dfab2061a9caa9ec743
Rohit Bachhav <bachhavrohit21@gmail.com>
2023-10-20 11:50:38 +0000

    changes in nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { UserButton } from '@clerk/nextjs'
 const Navbar = () => {
   return (
-    <div  className=' flex items-center justify-between p-4 shadow-sm border'>
+    <div  className=' flex items-center justify-between p-4 shadow-2xl'>
       <Image src='/design.png' alt="logo" width={80} height={70} className='rounded-2xl' />
       
       <div className='flex '> <UserButton/></div>
